92d06896e0bb9145f7bd95a5a4bbcb3e2f7d8d67
SHIN SUJUNG <shinssjj79@gmail.com>
2024-11-01 21:26:39 +0900

    Initial commit

---
+++ src/test/java/com/example/mini_chat/MiniChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mini_chat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
